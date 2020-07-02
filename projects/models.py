ca9da996c9d0e18595f3e93bf999667237201e4b
Ilya Rogers <ikrogers92@gmail.com>
2020-07-02 11:05:00 -0400

    Model and db creation

---
+++ projects/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here. Think of it as making a db tables
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FilePathField(path="/img")
